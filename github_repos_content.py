dfd2b5dfbd303f76c0db62951b1d87fbd56624bf
amincoding <aminobro70@gmail.com>
2021-03-24 14:56:13 +0000

    master

---
+++ github_repos_content.py
@@ -0,0 +1,13 @@
+# A programe made by amin abdedaiem 
+#
+# 14:15 wednesday the 24th 2021
+#
+# find out what a repository - of any one in github - contains 
+#
+# buy just providing his user name 
+#
+###################################################################
+ 
+
+# ask the user for the user_name
+user_name = input("Enter the repository username : ")
